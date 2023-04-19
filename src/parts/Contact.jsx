b77338470d96dcc011f8e9c1144c39697e287a11
Sortoite <zachatatem@gmail.com>
2023-04-18 20:16:27 -0400

    Feat: update dynamic animation

--- src/parts/Contact.jsx
+++ src/parts/Contact.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import ContactForm from "../components/ContactForm";
+import React from "react";import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
   return (
-    <div className="container flex flex-col justify-center items-center pt-20 md:pt-32">
-      <div className=" text-center text-5xl md:text-9xl font-bold text-gray-100 h-[170px] lg:h-[450px]">
+    <div className="container flex flex-col items-center justify-center pt-20 md:pt-32">
+      <div
+        className=" h-[170px] text-center text-5xl font-bold text-gray-100 md:text-9xl lg:h-[450px]"
+        data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+      >
         Contact
       </div>
-      <h2 className="m-1 text-center text-3xl lg:text-6xl font-extrabold font-['AvenirNextBold'] text-gray-900">
+      <h2 className="m-1 text-center font-['AvenirNextBold'] text-3xl font-extrabold text-gray-900 lg:text-6xl">
         Let's <span className="text-gray-100">get in</span> touch
       </h2>
       <ContactForm />
