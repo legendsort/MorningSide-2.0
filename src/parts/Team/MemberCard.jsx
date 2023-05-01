3c9e63986101c8afd50b17cf01d96ac51e758f18
Sortoite <zachatatem@gmail.com>
2023-05-01 08:35:00 -0400

    Fix: image padding:

--- src/parts/Team/MemberCard.jsx
+++ src/parts/Team/MemberCard.jsx
@@ -2,7 +2,7 @@ const Card = (props) => {
   const { name, photo, role, index } = props;
   return (
     <div className="col-span-3 md:col-span-1 border-r-3">
-      <div className={`bg-orange-color p-12 rounded-[30px] `}>
+      <div className={`bg-orange-color px-12 rounded-[30px] `}>
         <img className={`team`} src={photo} alt="logo" />
       </div>
       <div className="pt-5 font-['SpaceGrotesk'] text-[32px] font-bold uppercase text-main-color text-center md:text-left">
